c24d02199edd60741be55cecd5864484dbcbe252
Jake <jmp2814@gmail.com>
2022-08-22 11:47:17 -0400

    confirm PORT is working

--- Develop/server.js
+++ Develop/server.js
@@ -18,3 +18,7 @@ app.get('/api/notes', (req, res) => res.json(noteData));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost:${PORT}`));
