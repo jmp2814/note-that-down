6b525f8779ca2e1528bf126822197c65f356b116
Jake <jmp2814@gmail.com>
2022-08-22 11:38:12 -0400

    initial commit with server.js in progress

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const noteData = require('./db/db.json');
+const uuid = require('./helpers/uuid.js');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => res.json(noteData)); 
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
